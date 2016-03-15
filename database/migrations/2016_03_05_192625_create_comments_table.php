7d65a25c3160835a3559ceb9f8326dee189d0970
winniechenwenxin <winniechenwenxin@hotmail.com>
2016-03-15 16:31:44 +0800

    Database Migration Update

---
+++ database/migrations/2016_03_05_192625_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('comment_id');
+            $table->integer('user_id')->index();
+            $table->integer('post_id');
+            $table->string('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
