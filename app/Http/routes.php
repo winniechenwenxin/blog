5ec3c74c5cbca9da0b16ebeaa7addb2c589df06d
winniechenwenxin <winniechenwenxin@hotmail.com>
2016-03-15 18:30:12 +0800

    Delete Post Function updated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,8 @@ Route::group(['middleware' => ['web']], function () {
         // /post/{id}
         Route::get('{id}', 'PostController@getPost');
         Route::post('/create', 'PostController@store');
+        Route::delete('/{post}', 'PostController@deletePosts'); // delete
 
-        Route::post('{id}', 'PostController@postEditPost'); // edit
         Route::post('{id}/comment', 'CommentController@store'); // comment
         //Route::delete('{id}/comment/{comment_id}', 'CommentController@delete'); // delete comment
         Route::post('{id}/comment/{comment_id}/delete', 'CommentController@delete'); // delete comment
