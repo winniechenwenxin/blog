586b77c382dea4bc2f917739e80bd3ef6ca88bc4
winniechenwenxin <winniechenwenxin@hotmail.com>
2016-03-16 01:10:28 +0800

    Login and Register Redirection Implemented

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/tasks';
+    protected $redirectTo = '/post';
 
     /**
      * Create a new authentication controller instance.
